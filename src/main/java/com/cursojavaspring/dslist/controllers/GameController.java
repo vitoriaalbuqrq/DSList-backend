7b1b6d5cc67af87003087f43a9c66f2d5f088219
Vitoria Albuquerque <albuquerquevitoria14@gmail.com>
2023-05-16 01:09:56 -0300

    GET game by id

--- src/main/java/com/cursojavaspring/dslist/controllers/GameController.java
+++ src/main/java/com/cursojavaspring/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cursojavaspring.dslist.dto.GameDTO;
 import com.cursojavaspring.dslist.dto.GameMinDTO;
 import com.cursojavaspring.dslist.services.GameService;
 
@@ -17,6 +19,12 @@ public class GameController {
 	@Autowired
 	private GameService gameService;
 	
+	@GetMapping(value = "/{id}")
+	public GameDTO findAll(@PathVariable Long id){
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
+
 	@GetMapping
 	public List<GameMinDTO> findAll(){
 		List<GameMinDTO> result = gameService.findAll();
